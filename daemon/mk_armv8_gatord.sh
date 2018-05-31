e517d7cb53056d9490a0d5625f1fc871949be1ce
jianxin.pan <jianxin.pan@amlogic.com>
2018-05-31 15:16:50 +0800

    add mk.sh

---
+++ daemon/mk_armv8_gatord.sh
@@ -0,0 +1,1 @@
+make -f Makefile_aarch64 CROSS_COMPILE=aarch64-linux-gnu- LDFLAGS=-static
---
+++ driver/mk_armv8.sh
@@ -0,0 +1,1 @@
+make -C /mnt/fileroot/jianxin.pan/g_bringup/common_49_ing M=`pwd` ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules
